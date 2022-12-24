f3e7e0d1f8646e31bc230c40d7460fb8164c9fce
akash <akashsarki12345@gmail.com>
2022-12-24 19:28:00 +0530

    added port

--- server/server.js
+++ server/server.js
@@ -43,6 +43,6 @@ app.post('/', async (req, res) => {
     }
 })
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`server ready on http://localhost:5000`);
 })
